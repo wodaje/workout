5a7362d04191e8a80a4b52f9828092410e1a14e8
Jeff Woda <wodahouse@gmail.com>
2021-03-10 20:36:38 -0500

    adding server file

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const logger = require("morgan")
+const mongoose = require("mongoose")
+const path = require("path");
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+app.use(logger("dev"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")))
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+}).then(() => console.log("Database is Connected"))
+  .catch(err => console.log(err))
+
+require("./routes/api-routes.js")(app)
+require("./routes/html-routes.js")(app)
+
+app.listen(PORT, () => {
+  console.log(`
+   ==> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.
+  `)
+})
