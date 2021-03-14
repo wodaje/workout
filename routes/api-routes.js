0a7536afccf2475a2ecddc2fa9469e3f05c98404
Jeff Woda <wodahouse@gmail.com>
2021-03-13 19:26:23 -0500

    updated routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,45 @@
+const router = require("express").Router()
+const { request } = require("http")
+const { Workout } = require("../models")
 
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([{ $addFields: { totalDuration: { $sum: "$exercises.duration" }}}])
+  .then(dbTransaction => {
+    res.json(dbTransaction)
+  })
+  .catch(err => {
+    res.status(400).json(err)
+  })
+})
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}})
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    })
+})
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    })
+})
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([{ $addFields: { totalDuration: { $sum: "$exercises.duration" }}}]).sort({_id: -1}).limit(7)
+  .then(dbWorkout => {
+    res.json(dbWorkout)
+  })
+  .catch(err => {
+    res.status(400).json(err)
+  })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express")
-const logger = require("morgan")
 const mongoose = require("mongoose")
 const path = require("path");
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
-app.use(logger("dev"))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "public")))
@@ -16,11 +15,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 }).then(() => console.log("Database is Connected"))
   .catch(err => console.log(err))
 
-// require("./routes/api-routes.js")(app)
-// require("./routes/html-routes.js")(app)
+ app.use(require("./routes/api-routes.js"))
+ app.use(require("./routes/html-routes.js"))
 
 app.listen(PORT, () => {
-  console.log(`
-   ==> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.
-  `)
+  console.log(` Listening on port ${PORT}`)
 })
