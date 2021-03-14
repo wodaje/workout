5a5ecfa08ad23887ea5f54d3bcdaf5de636c88fa
Jeff Woda <wodahouse@gmail.com>
2021-03-13 20:05:14 -0500

    fixed styling

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,45 +2,44 @@ const router = require("express").Router()
 const { request } = require("http")
 const { Workout } = require("../models")
 
-
 router.put("/api/workouts/:id", (req, res) => {
   Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}})
     .then(dbWorkout => {
       res.json(dbWorkout)
-    })
+      })
     .catch(err => {
       res.status(400).json(err)
-    })
+      })
 })
 
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
     .then(dbWorkout => {
       res.json(dbWorkout)
-    })
+      })
     .catch(err => {
       res.status(400).json(err)
-    })
+      })
 })
 
 router.get("/api/workouts", (req, res) => {
   Workout.aggregate([{ $addFields: { totalDuration: { $sum: "$exercises.duration" }}}])
-  .then(dbTransaction => {
-    res.json(dbTransaction)
-  })
-  .catch(err => {
+   .then(dbTransaction => {
+     res.json(dbTransaction)
+     })
+    .catch(err => {
     res.status(400).json(err)
-  })
+      })
 })
 
 router.get("/api/workouts/range", (req, res) => {
   Workout.aggregate([{ $addFields: { totalDuration: { $sum: "$exercises.duration" }}}]).sort({_id: -1}).limit(7)
-  .then(dbWorkout => {
+    .then(dbWorkout => {
     res.json(dbWorkout)
-  })
-  .catch(err => {
+      })
+    .catch(err => {
     res.status(400).json(err)
-  })
+      })
 })
 
 module.exports = router
